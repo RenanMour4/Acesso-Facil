ac18247814b8e1ac0bcb6920c599c03e75fdf72d
João Vitor Brandão <64711434+joao0902@users.noreply.github.com>
2022-11-07 19:20:04 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,9 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+})
